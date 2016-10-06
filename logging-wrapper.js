3b98560e431d2615dffd1d3291f1351124c39a39
TAE YOUNG (Daniel) KIM <ty2kim@gmail.com>
2016-10-06 19:26:23 +0000

    second commit of logging-wrapper.js

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -1,5 +1,7 @@
 var wrapLog = function (callback, name) {
+  //console.log(arguments); // { '0': [Function], '1': 'sum' }
   return function(){
+    //console.log(arguments); // { '0': 5, '1': 3 }
     var sum = callback(arguments[0], arguments[1]);
     console.log(`${name}(${arguments[0]},${arguments[1]}) => ${sum}`);
   };
@@ -7,6 +9,12 @@ var wrapLog = function (callback, name) {
 
 var sum = function (a,b) { return a + b; };
 var logSum = wrapLog(sum, "sum");
+
+// logSum = function(){
+//   console.log(arguments); // { '0': 5, '1': 3 }
+//   var sum = callback(arguments[0], arguments[1]);
+//   console.log(`${name}(${arguments[0]},${arguments[1]}) => ${sum}`);
+// };
 
 logSum(5,3); // sum(5,3) => 8
 logSum(3,2); // sum(3,2) => 5
