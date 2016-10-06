498781d0515b67286129f707922b0af973d2c2da
TAE YOUNG (Daniel) KIM <ty2kim@gmail.com>
2016-10-06 18:18:54 +0000

    first commit of loadedDie.js and countdownGenerator

---
+++ closure.js
@@ -0,0 +1,58 @@
+// var nextId = (function () {
+//   var id = 0;
+//
+//   return function () {
+//     id += 1;
+//     return id;
+//   }
+// })();
+//
+// console.log(nextId()); // 1
+// console.log(nextId()); // 2
+//
+// var nextId = function () {
+//   var id = 0;
+//
+//   return function () {
+//     id += 1;
+//     return id;
+//   }
+// };
+//
+// console.log(nextId()); // function
+// console.log(nextId()); // function
+
+var loadedDie = (function () {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  var idx = -1;
+  return function () {
+    idx += 1;
+    return list[idx];
+  };
+})();
+
+// console.log(loadedDie());  // 5
+// console.log(loadedDie());  // 4
+// console.log(loadedDie());  // 6
+
+var countdownGenerator = function (x) {
+  var count = x;
+  return function(){
+    if(count === 0){
+      console.log("Blast Off!");
+    }
+    else if(count < 0){
+      console.log("Rockets already gone, bub!");
+    }
+    else console.log(`T-minus ${count}...`);
+    count--;
+  };
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
